f64524817738662f6c63fbe6dca424970b659c55
kurages <kurages@kurages.dev>
2025-12-13 05:02:51 +0000

    resolverをインタラクションタイプごとに定義したい

--- packages/discord-interaction-client/src/interaction-handler/handler.ts
+++ packages/discord-interaction-client/src/interaction-handler/handler.ts
@@ -4,32 +4,61 @@ import {
   InteractionType,
 } from "../types";
 
-export interface BaseInteractionHandler<Type extends InteractionType> {
+export interface BaseInteractionHandler<
+  Type extends InteractionType,
+  Deferred extends Boolean = false,
+> {
   handle(
     component: InteractionRequest[Type],
-  ): Promise<InteractionResponse[Type]>;
+  ): Deferred extends true
+    ? Promise<[InteractionResponse[Type], Promise<InteractionResponse[Type]>]>
+    : Promise<InteractionResponse[Type]>;
 }
 
-export interface PingHandler extends BaseInteractionHandler<InteractionType.Ping> {}
-
-export interface ApplicationCommandHandler extends BaseInteractionHandler<InteractionType.ApplicationCommand> {
+interface BaseApplicationCommandHandler<
+  Deferred extends Boolean,
+> extends BaseInteractionHandler<InteractionType.ApplicationCommand, Deferred> {
   name: string;
   description: string;
 }
 
-export interface MessageComponentHandler extends BaseInteractionHandler<InteractionType.MessageComponent> {}
-export interface AutocompleteHandler extends BaseInteractionHandler<InteractionType.ApplicationCommandAutocomplete> {}
-export interface ModalSubmitHandler extends BaseInteractionHandler<InteractionType.ModalSubmit> {}
+interface BaseMessageComponentHandler<
+  Deferred extends Boolean,
+> extends BaseInteractionHandler<InteractionType.MessageComponent, Deferred> {}
+
+interface BaseAutocompleteHandler<
+  Deferred extends Boolean,
+> extends BaseInteractionHandler<
+  InteractionType.ApplicationCommandAutocomplete,
+  Deferred
+> {}
+
+interface BaseModalSubmitHandler<
+  Deferred extends Boolean,
+> extends BaseInteractionHandler<InteractionType.ModalSubmit, Deferred> {}
+
+export interface PingHandler extends BaseInteractionHandler<
+  InteractionType.Ping,
+  false
+> {}
+export interface ApplicationCommandHandler extends BaseApplicationCommandHandler<false> {}
+export interface DeferredApplicationCommandHandler extends BaseApplicationCommandHandler<true> {}
+export interface MessageComponentHandler extends BaseMessageComponentHandler<false> {}
+export interface DeferredMessageComponentHandler extends BaseMessageComponentHandler<true> {}
+export interface AutocompleteHandler extends BaseAutocompleteHandler<false> {}
+export interface DeferredAutocompleteHandler extends BaseAutocompleteHandler<true> {}
+export interface ModalSubmitHandler extends BaseModalSubmitHandler<false> {}
+export interface DeferredModalSubmitHandler extends BaseModalSubmitHandler<true> {}
 
 export type InteractionHandler<K extends InteractionType> =
   K extends InteractionType.Ping
     ? PingHandler
     : K extends InteractionType.ApplicationCommand
-      ? ApplicationCommandHandler
+      ? ApplicationCommandHandler | DeferredApplicationCommandHandler
       : K extends InteractionType.MessageComponent
-        ? MessageComponentHandler
+        ? MessageComponentHandler | DeferredMessageComponentHandler
         : K extends InteractionType.ApplicationCommandAutocomplete
-          ? AutocompleteHandler
+          ? AutocompleteHandler | DeferredAutocompleteHandler
           : K extends InteractionType.ModalSubmit
-            ? ModalSubmitHandler
+            ? ModalSubmitHandler | DeferredModalSubmitHandler
             : never;
--- packages/discord-interaction-client/src/interaction-handler/resolver/simple-resolver.ts
+++ packages/discord-interaction-client/src/interaction-handler/resolver/simple-resolver.ts
@@ -1,60 +1,39 @@
-import { ApplicationCommandHandler, InteractionHandler } from "../handler";
 import { InteractionHandlerRegistry } from "../registry";
-import { InteractionRequest, InteractionType } from "../../types";
-import { InteractionHandlerResolver } from "./resolver";
+import { InteractionType } from "../../types";
+import {
+  DelegatingTypedInteractionHandlerResolver,
+  TypedHandlerResolver,
+} from "./resolver";
 
-export class SimpleInteractionHandlerResolver implements InteractionHandlerResolver {
-  constructor(private registry: InteractionHandlerRegistry) {}
-
-  private isMatch<T extends InteractionType>(
-    interaction: InteractionRequest[T],
-    handler: InteractionHandler<T>,
-  ): boolean {
-    if (interaction.type === InteractionType.ApplicationCommand) {
-      return (
-        (handler as ApplicationCommandHandler).name === interaction.data.name
-      );
-    }
-    // TODO implement other matching logic
-    throw new Error("unsupported interaction type");
-  }
-
-  findFirst<K extends InteractionType>(
-    interaction: InteractionRequest[K],
-  ): InteractionHandler<K> | null {
-    const handlers = this.registry.get(
-      interaction.type,
-    ) as InteractionHandler<K>[];
-    if (interaction.type === InteractionType.Ping) {
-      return handlers ? handlers[0] : null;
-    }
-
+class SimpleApplicationCommandHandlerContainer implements TypedHandlerResolver<InteractionType.ApplicationCommand> {
+  get(handlers, interaction) {
     for (const handler of handlers) {
-      if (this.isMatch(interaction, handler)) {
+      if (handler.name === interaction.data.name) {
         return handler;
       }
     }
-
     return null;
   }
 
-  find<K extends InteractionType>(
-    interaction: InteractionRequest[K],
-  ): Array<InteractionHandler<K>> | null {
-    const handlers = this.registry.get(
-      interaction.type,
-    ) as InteractionHandler<K>[];
-    if (interaction.type === InteractionType.Ping) {
-      return handlers;
-    }
+  getFirst(handlers, interaction) {
+    const handler = this.get(handlers, interaction);
+    return handler ? handler[0] : null;
+  }
+}
 
-    let result: Array<InteractionHandler<K>> = [];
-    for (const handler of handlers) {
-      if (this.isMatch(interaction, handler)) {
-        result.push(handler);
-      }
-    }
-
-    return result;
+/**
+ * シンプルなハンドラ解決を行う`InteractionHandlerResolver`の実装。
+ */
+export class SimpleInteractionHandlerResolver extends DelegatingTypedInteractionHandlerResolver {
+  constructor(registry: InteractionHandlerRegistry) {
+    const resolvers = new Map<
+      InteractionType,
+      TypedHandlerResolver<InteractionType>
+    >();
+    resolvers.set(
+      InteractionType.ApplicationCommand,
+      new SimpleApplicationCommandHandlerContainer(),
+    );
+    super(registry, resolvers);
   }
 }
